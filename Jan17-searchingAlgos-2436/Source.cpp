440524fc7e0f064133c687e49c28147b3ae2e8fb
Work <Work@MSI>
2024-01-17 13:08:20 -0600

    Where we ended on Jan 17

--- Jan17-searchingAlgos-2436/Source.cpp
+++ Jan17-searchingAlgos-2436/Source.cpp
@@ -1,10 +1,64 @@
 #include <iostream>
+#include <vector>
 
-using namespace std;
+//using namespace std;
+
+
+using std::cout; 
+using std::endl; 
+using std::vector; 
+
+
+template <typename T> 
+int sequential_search(vector<T> someList, T someTargetValue)
+{
+    for (int i = 0; i < someList.size(); i++)
+    {
+        if (someList.at(i) == someTargetValue) //replace with subscript operator [] (if desired) 
+        {
+            return i; 
+        }
+    }
+
+    return -1;
+}
+
+auto generateRandomVector(int N)
+{
+    vector<int> randomVector; 
+
+    for (int i = 0; i < N; i++)
+    {
+        randomVector.push_back(rand() % N);
+    }
+
+    return randomVector;
+}
+
+template<typename T> 
+void print(vector<T> list)
+{
+    for (auto& element : list)
+    {
+        cout << element << " "; //
+    }
+    cout << "\n";
+}
 
 int main()
 {
-    cout << "Hello World";
+    srand(time(0)); //a better way to do this - using 
+    vector<int> nums = generateRandomVector(10);
+    
+    int targetValue = rand() % 10; 
+
+    cout << "The randomly-generated vector of 10 elements is: " << endl; 
+    print(nums); 
+
+    int foundPosition = sequential_search(nums, targetValue); 
+    //std::find
+    cout << "The target value " << targetValue << "was? found at index number :"
+        << foundPosition << endl; 
 
     return 0;
 }
